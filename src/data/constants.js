d2617f4b0597dd1b7903161f3c6b05e6dca87e08
kravmagaipiranga <kravmagaipiranga@gmail.com>
2026-03-12 10:07:53 -0300

    Create src/data/constants.js with constant values

---
+++ src/data/constants.js
@@ -0,0 +1,13 @@
+const MOCK_USER = "User123";
+const RISK_COLORS = {
+    LOW: "#green",
+    MEDIUM: "#yellow",
+    HIGH: "#red"
+};
+const RISK_BG = {
+    LOW: "#lightgreen",
+    MEDIUM: "#lightyellow",
+    HIGH: "#lightcoral"
+};
+
+export { MOCK_USER, RISK_COLORS, RISK_BG };
